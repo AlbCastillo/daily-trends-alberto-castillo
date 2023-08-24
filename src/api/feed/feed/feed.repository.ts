43e3b5807175d74356a512b740856fbfbf36abc2
AlbCastillo <alberto.castillo@beoneoff.com>
2023-08-24 21:39:45 +0200

    feat: add feed repository

--- src/api/feed/feed/feed.repository.ts
+++ src/api/feed/feed/feed.repository.ts
@@ -1,25 +1,38 @@
 import { autoInjectable, singleton } from 'tsyringe';
 
 import { CreateFeedDto } from './dto/create-feed.dto';
+import { FindFeedInputDto } from './dto/find-feed.dto';
+import { UpdatedInputDto } from './dto/update-feed.dto';
 import FeedModel from './models/feed.model';
 import { FeedI } from './models/feed.schema';
 
 @singleton()
 @autoInjectable()
 export class FeedRepository {
-  async create(feedInput: CreateFeedDto): Promise<FeedI> {
-    return FeedModel.create(feedInput);
+  async create(createFeedInput: CreateFeedDto): Promise<FeedI> {
+    return FeedModel.create(createFeedInput);
   }
 
   async delete(id: string) {
     return FeedModel.findByIdAndDelete(id);
   }
 
+  async update(updateFeedInput: UpdatedInputDto): Promise<FeedI | null> {
+    return FeedModel.findOneAndUpdate(
+      { _id: updateFeedInput.id },
+      updateFeedInput.articles,
+    );
+  }
+
   async get(id: string) {
     return FeedModel.findById(id);
   }
 
-  async findAll() {
-    return FeedModel.find();
+  async findAll(search: FindFeedInputDto = {}) {
+    return FeedModel.find(search);
+  }
+
+  async count(search: FindFeedInputDto = {}) {
+    return FeedModel.count(search);
   }
 }
