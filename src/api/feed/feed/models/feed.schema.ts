1d7ec6f3a13e835c0ec7d6334fb27ea0ffb206b7
AlbCastillo <alberto.castillo@beoneoff.com>
2023-08-24 21:23:43 +0200

    feat: add feed schema

--- src/api/feed/feed/models/feed.schema.ts
+++ src/api/feed/feed/models/feed.schema.ts
@@ -1,13 +1,32 @@
 import { Schema, Document } from 'mongoose';
-
+export interface ArticleI {
+  title: string;
+  content: string;
+  link: string;
+}
 export interface FeedI extends Document {
   name: string;
+  newspaper: string;
+  articles: ArticleI[];
+  createdAt: string;
 }
 
-export const FeedSchema: Schema<FeedI> = new Schema<FeedI>({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
+export const FeedSchema: Schema<FeedI> = new Schema<FeedI>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      default: () => `Daily news ${new Date().toUTCString()}`,
+    },
+    newspaper: {
+      type: String,
+    },
+    articles: [
+      {
+        type: Schema.Types.Mixed,
+      },
+    ],
   },
-});
+  { timestamps: true },
+);
